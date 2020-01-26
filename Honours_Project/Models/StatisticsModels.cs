de33feb6e2cc8d6729cbffa929379413f4232597
rtaggart16 <rosstaggart44@gmail.com>
2020-01-26 14:29:29 +0000

    Implemented addition score calculation

---
+++ Honours_Project/Models/StatisticsModels.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Honours_Project.Models
+{
+    //! Section: Contents
+
+    /*
+     * - Top-level models
+     *      - Contribution_Request
+     *      - Contributuon_Result
+     * - Sub-level models
+     *      - Repo_Total
+     *      - Score_Component
+    */
+
+    //! END Section: Contents
+
+
+    //! Section: Top-level models
+
+    public class Contribution_Request
+    {
+        public Repo_Total Repo { get; set; }
+
+        public Repo_Total User { get; set; }
+
+        public int Author_Total { get; set; }
+    }
+
+    public class Contribution_Result
+    {
+        public Score_Component Score { get; set; }
+    }
+
+    //! END Section: Top-level models
+
+
+    //! Section: Sub-level models
+
+    public class Repo_Total
+    {
+        public int Commit_Total { get; set; }
+
+        public int Addition_Total { get; set; }
+
+        public int Deletion_Total { get; set; }
+    }
+
+    public class Score_Component
+    {
+        public decimal Commit_Score { get; set; }
+
+        public decimal Addition_Score { get; set; }
+
+        public decimal Deletion_Score { get; set; }
+
+        public decimal Contribution_Score { get; set; }
+    }
+
+    //! END Section: Sub-level models
+}
---
+++ Honours_Project/Services/IStatisticsService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Honours_Project.Services
+{
+    public interface IStatisticsService
+    {
+    }
+
+    public class StatisticsService : IStatisticsService
+    {
+        private readonly decimal ADDITION_PERCENTAGE_FACTOR = 50M;
+
+        //! SECTION: Top-level Calculation
+
+
+        //! END SECTION: Top-level Calculations
+
+
+        //! SECTION: Sub-level Calculations
+        
+        /// <summary>
+        /// Method to calculate an individual's addition score
+        /// </summary>
+        /// <param name="userAdditions"></param>
+        /// <param name="repoAdditions"></param>
+        /// <param name="authorTotal"></param>
+        /// <returns></returns>
+        public decimal Calculate_Addition_Score(int userAdditions, int repoAdditions, int authorTotal)
+        {
+            // If there are no additions in the repo, provide an equal distribution to all collaborators
+            if (repoAdditions == 0)
+            {
+                return (ADDITION_PERCENTAGE_FACTOR / (decimal)authorTotal);
+            }
+
+            return ((decimal)userAdditions / (decimal)repoAdditions) * ADDITION_PERCENTAGE_FACTOR;
+        }
+
+        //! END SECTION: Sub-level Calculations
+    }
+}
